a96a18f45d192c3f48edea1c5b1fceaf30cc0446
Mohit Rane <mohitsrane16@gmail.com>
2024-01-18 15:03:47 +0530

    v1 blogging add

---
+++ 6_blogging-app/version/v1/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateBlogComponent } from './pages/create-blog/create-blog.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ViewBlogComponent } from './pages/view-blog/view-blog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopbarComponent,
+    HomeComponent,
+    CreateBlogComponent,
+    ViewBlogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    AngularEditorModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
